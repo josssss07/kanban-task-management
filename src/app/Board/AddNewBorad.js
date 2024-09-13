840c26f5031d8a90374fddf57a2d73e57cc65ef7
AdleenaLobo <1069455+AdleenaLobo@users.noreply.github.com>
2024-09-13 20:46:03 +0530

    Add toggle to change color theme

--- src/app/Board/AddNewBorad.js
+++ src/app/Board/AddNewBorad.js
@@ -2,19 +2,22 @@
 import CustomDialog from "../../../components/Dialog/Dialog.js";
 import Button from "../../../components/Button/Button.js";
 export default function AddNewBoard({open , onChange}) {
+  const heightx=400;
+  const heighty=500;
   return (
     <div>
       <CustomDialog 
       open ={open}
       onChange={onChange}
       title={"Add New Board"}
+      heightx={heightx} heighty={heighty}
       >
         <form>
           <label className="text-body-m text-medium-grey">Name</label><br/>
-          <input type="text" className="w-full text-body-l p-2 border-2 border-[var(--color-lineinput)] rounded-md" placeholder="eg: Web Design"></input><br/>
+          <input type="text" className="w-full text-body-l p-2 border-2 border-[var(--color-lineinput)] bg-[var(--color-dialog)] rounded-md" placeholder="eg: Web Design"></input><br/>
           <label className="text-body-m text-medium-grey">Cloumns</label><br/>
-          <div><input type="text" placeholder="Todo" readOnly  className="w-5/6  text-body-l p-2 border-2 border-[var(--color-lineinput)] rounded-md" ></input><Button textColor={"text-medium-grey"} bgColor={"bg-none"} className={"text-heading-l"}>X</Button></div>
-          <div><input type="text" placeholder="Doing" readOnly  className="w-5/6  text-body-l p-2 border-2 border-[var(--color-lineinput)] rounded-md" ></input><Button  textColor={"text-medium-grey"} bgColor={"bg-none"} className={"text-heading-l"}>X</Button></div>
+          <div><input type="text" placeholder="Todo" readOnly  className="w-5/6  text-body-l p-2 border-2 border-[var(--color-lineinput)] rounded-md bg-[var(--color-dialog)]" ></input><Button textColor={"text-medium-grey"} bgColor={"bg-none"} className={"text-heading-l"}>X</Button></div>
+          <div><input type="text" placeholder="Doing" readOnly  className="w-5/6  text-body-l p-2 border-2 border-[var(--color-lineinput)] rounded-md bg-[var(--color-dialog)]" ></input><Button  textColor={"text-medium-grey"} bgColor={"bg-none"} className={"text-heading-l"}>X</Button></div>
           <Button className="w-full" textColor={"text-main-purple"} bgColor={"bg-lines-light"}>+Add New Cloumn</Button><br/>
           <input type="submit" value="Create New Board" className="text-white bg-main-purple rounded-full m-2 p-1 w-full"/>
         </form>
--- src/app/Helpers/EyeBunOrNavBar.js
+++ src/app/Helpers/EyeBunOrNavBar.js
@@ -1,16 +1,59 @@
 "use client";
 
-import React from "react";
+import React, { useState , useEffect } from "react";
 import EyeButton from "../../../components/EyeButton/EyeButton";
 import NavBar from "../../../components/NavBar/NavBar";
 import PageSection from "../../../components/NavBar/PageSection";
 import { Sun, Moon, EyeOff } from "feather-icons-react";
-import Button from "../../../components/Button/Button";
 import { DisplayNavContext } from "./DisplayNavContext";
 import { useContext } from "react";
+import AddNewBoard from "../Board/AddNewBorad";
+import { Light_colors, Dark_colors } from "../constants";
 
 export default function EyeOrNav() {
+  var colorVar = null;
+  const [colorTheme , setColorTheme]= useState(null);
+  useEffect(()=>{
+     colorVar = getComputedStyle(document.documentElement).getPropertyValue('--color-prefer').trim();
+     setColorTheme(colorVar);
+     
+  },[]);
+
+  useEffect(()=>{
+    
+    console.log(colorTheme);
+    toggleTheme();
+  }, [colorTheme]);
+
+  function toggleTheme(){
+    console.log(colorTheme);
+    if(colorTheme == "light"){
+      const color = Light_colors;
+      document.documentElement.style.setProperty('--color-prefer', '"light"');
+      document.documentElement.style.setProperty('--color-text', color.text);
+      document.documentElement.style.setProperty('--color-background', color.background);
+      document.documentElement.style.setProperty('--color-dialog', color.dialog);
+      document.documentElement.style.setProperty('--color-backgroundlighter', color.backgroundlighter);
+      document.documentElement.style.setProperty('--color-lineborder', color.lineborder);
+      document.documentElement.style.setProperty('--color-lineinput', color.lineinput);
+
+    }else if(colorTheme == "dark"){
+      
+      const color = Dark_colors;
+      document.documentElement.style.setProperty('--color-prefer', '"light"');
+      document.documentElement.style.setProperty('--color-text', color.text);
+      document.documentElement.style.setProperty('--color-background', color.background);
+      document.documentElement.style.setProperty('--color-dialog', color.dialog);
+      document.documentElement.style.setProperty('--color-backgroundlighter', color.backgroundlighter);
+      document.documentElement.style.setProperty('--color-lineborder', color.lineborder);
+      document.documentElement.style.setProperty('--color-lineinput', color.lineinput);
+
+    }else{
+      console.log("null");
+    }
+  }
   const [displayNav, setDisplayNav] = useContext(DisplayNavContext);
+  const [newBoard , setNewBoard] = useState(false);
 
   function setDisplay() {
     setDisplayNav(!displayNav);
@@ -28,16 +71,21 @@ export default function EyeOrNav() {
           <PageSection styles={"text-medium-grey"}>Platform Launch</PageSection>
           <PageSection styles={"text-medium-grey"}>Marketing Plan</PageSection>
           <PageSection styles={"text-medium-grey"}>Roadmap</PageSection>
-          <PageSection styles={"text-main-purple"}>
+          <PageSection styles={"text-main-purple"} state={newBoard} onChange={setNewBoard}>
             +Create New Board
           </PageSection>
+          {newBoard? <AddNewBoard open={newBoard} onChange={setNewBoard}/>: undefined}
           <div className="mt-auto mb-4">
             <div className="flex justify-center gap-6 items-center">
               <div  className="flex px-10 gap-6 bg-[var(--color-backgroundlighter)] p-2">
               <Sun fill={"grey"} style={{stroke:"grey"}} size={20}/>
               <div className="m-0 p-1 flex bg-main-purple rounded-full">
-                <Button className={"min-w-4 h-4 mr-0 my-0 ml-0 px-0 py-0 bg-white"} />
-                <Button className={"w-4 h-4 ml-0 my-0 px-0 py-0"} />
+              <label className="relative inline-block w-[55px] h-[24px]">
+                <input type="checkbox" className=" w-0 h-0 peer" onChange={()=>{
+                  colorTheme=="light"?setColorTheme("dark"):setColorTheme("light");
+                }}/>
+                  <span className="absolute  top-0 left-0 right-0 bottom-0 cursor-pointer bg-white duration-[0.4s] w-[24px] rounded-full h-[24px] peer-checked:translate-x-8"></span>
+              </label>
                 </div>
               <Moon fill={"grey"} style={{stroke:"grey"}} size={20}/>
               </div>
